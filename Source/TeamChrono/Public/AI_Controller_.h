09b0145c52bd96cb2cc1e8d31f6a911601052143
20191143namminwoo <mvpalsdn771@gmail.com>
2024-01-17 00:30:16 +0900

    BT 작업중

---
+++ Source/TeamChrono/Public/AI_Controller_.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "AI_Controller_.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAMCHRONO_API AAI_Controller_ : public AAIController
+{
+	GENERATED_BODY()
+	
+};
