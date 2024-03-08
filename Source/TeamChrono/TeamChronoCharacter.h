28ea330ac43044dd7fed053da8347740586c8bd9
20191143namminwoo <mvpalsdn771@gmail.com>
2024-03-09 00:27:54 +0900

    Revert "Merge branch 'RetargetingAni_Joyeong' into MonsterBase_Minwoo"
    This reverts commit 527ec4a95e85534cc90747fb01db3003c45d0239, reversing
    changes made to c5d78f3d6d77461cc97b21bfaa59dcae06de6a11.

--- Source/TeamChrono/TeamChronoCharacter.h
+++ Source/TeamChrono/TeamChronoCharacter.h
@@ -44,9 +44,6 @@ class ATeamChronoCharacter : public ACharacter
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	UInputAction* LookAction;
 
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
-	UInputAction* ActtackAction;
-
 	UPROPERTY(VisibleAnywhere)
 	class UWidgetComponent* StaminaBar;
 
@@ -79,10 +76,10 @@ public:
 
 	//최대 스테미너
 	UPROPERTY(EditAnywhere, Category = "Stamina")
-	float pcMaxStamina = 100.0f;
+	float pcMaxStamina = 100;
 	// 현재 스테미너
 	UPROPERTY(EditAnywhere, Category = "Stamina")
-	float pcStamina = 100.0f;
+	float pcStamina = 100;
 	// 스테미너UI 부드럽게 변경 시간
 	UPROPERTY(EditAnywhere, Category = "Stamina")
 	float pcStaminaTimer = 0.05f;
