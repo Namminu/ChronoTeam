b1b7bac63183a27ed0157b7812c0d378a05bfcb2
rootinge <jhpark5129@gmail.com>
2024-03-14 22:37:40 +0900

    마우스 커서 보이게

--- Source/TeamChrono/TeamChronoGameMode.cpp
+++ Source/TeamChrono/TeamChronoGameMode.cpp
@@ -3,11 +3,12 @@
 #include "TeamChronoGameMode.h"
 #include "TeamChronoCharacter.h"
 #include "UObject/ConstructorHelpers.h"
-
+#include "MyPlayerController.h"
 ATeamChronoGameMode::ATeamChronoGameMode()
 {
 	// set default pawn class to our Blueprinted character
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	PlayerControllerClass = AMyPlayerController::StaticClass();
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
