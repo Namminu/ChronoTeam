31790fb6ba49cfbfccf9d5364e26e394ae632d93
20191143namminwoo <mvpalsdn771@gmail.com>
2024-04-26 22:32:42 +0900

    MapChanger 작업
    1. BP_NotifierDoor : 처음 overlap 시 몬스터 Spawn 시작, 할당된 몬스터 전부 잡아야 Open
    2. BP_Spawner : SpawnCount 만큼 할당된 몬스터 반복 생성, 이전 생성된 몬스터 죽어야 새로 Spawn
    3. BP_NextDoor : NotifierDoor 에 연결 -> Open / Close 때 같이 작동

---
+++ Source/TeamChrono/Private/NotifierDoor.cpp
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NotifierDoor.h"
+#include "Components/BoxComponent.h"
+#include "MonsterSpawner.h"
+#include "DoorNextDoor.h"
+
+// Sets default values
+ANotifierDoor::ANotifierDoor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootScene = CreateDefaultSubobject<USceneComponent>(TEXT("Root Scene"));
+	RootComponent = RootScene;
+
+	BoxColl = CreateDefaultSubobject<UBoxComponent>(TEXT("Box Coll"));
+	BoxColl->SetupAttachment(RootScene);
+}
+
+// Called when the game starts or when spawned
+void ANotifierDoor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	isGetWorked = false;
+
+	BoxColl->OnComponentEndOverlap.AddDynamic(this, &ANotifierDoor::OnOverlapEnd);
+}
+
+// Called every frame
+void ANotifierDoor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (isGetWorked)
+	{
+		if (MonsterArray.Num() == 0)
+		{
+			SetOpenDoor();
+			SetActorTickEnabled(false);
+		}
+	}
+}
+
+void ANotifierDoor::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, 
+	AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (OtherActor->ActorHasTag("PLAYER"))
+	{
+		if (!isGetWorked)
+		{
+			for (AMonsterSpawner* Spawner : SpawnerArray)
+			{
+				Spawner->SpawnMonster();
+			}
+			isGetWorked = true;
+			SetCloseDoor();
+		}
+
+	}
+}
+
