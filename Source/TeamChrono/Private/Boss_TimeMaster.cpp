59931947416cf8f6f8c846f6cd15a1cf4cab418b
20191143namminwoo <mvpalsdn771@gmail.com>
2024-05-02 01:04:58 +0900

    Chrono 작업중 (1)
    1. Take Damage / Damage Flash 완료
    2. Attack Func (기본 공격 용) / Gimic Func (기믹용)
    	=> GetRandomNum() 으로 랜덤 호출
    3. 기본 뼈대 설립 : 추후 수정 가능

--- Source/TeamChrono/Private/Boss_TimeMaster.cpp
+++ Source/TeamChrono/Private/Boss_TimeMaster.cpp
@@ -8,21 +8,133 @@ ABoss_TimeMaster::ABoss_TimeMaster()
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	sk_Halo = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Halo"));
+	sk_Halo->SetupAttachment(GetMesh());
+	sk_Halo->SetLeaderPoseComponent(GetMesh());
 }
 
 void ABoss_TimeMaster::BeginPlay()
 {
 	Super::BeginPlay();
 
+	// Reset Flash Material For Damage Flash
+	SetFlashMT(GetMesh(), 0);
+	// Reset Every Attack Count Properties
+	cur_StrikeCount = 0;
+	cur_SkillCount = 0;
+	// Reset Checking Attack Type Properties
+	bIsAttack = false;
+	bIsGimic = false;
+	// Reset Current Boss Pase
+	CurrentPase = 1;
 }
 
 void ABoss_TimeMaster::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (!bIsAttack&&!bIsGimic)
+	{
+		SetFarfromPlayer(DistanceToPlayer);
+	}
 }
 
 void ABoss_TimeMaster::EndPlay(const EEndPlayReason::Type EndPlayReason)
 {
 	Super::EndPlay(EndPlayReason);
+}
+
+int ABoss_TimeMaster::GetRandomAttackNum(int num)
+{
+	return FMath::RandRange(0, num);
+}
+
+void ABoss_TimeMaster::SetFlashMT(USkeletalMeshComponent* skeleton, int index)
+{
+	FlashMT = skeleton->CreateDynamicMaterialInstance(index);
+	FlashMT->GetVectorParameterValue(TEXT("EmissiveColor"), InitColor);
+	FlashMT->GetScalarParameterValue(TEXT("EmissiveMutiplier"), InitMultiplier);
+}
+
+void ABoss_TimeMaster::CheckCurrentPase()
+{
+	//if(GetBossCurrentHp())
+}
+
+int ABoss_TimeMaster::MeleeAttack_Implementation()
+{
+	// Default Normal Attack / Not Strike, Not Gimic
+	if (cur_StrikeCount < max_StrikeCount && cur_SkillCount < max_SkillCount)
+	{
+		bIsAttack = true;
+
+		AttackFunc(GetRandomAttackNum(NormalAttackTotalCount));
+		cur_StrikeCount++;
+		cur_SkillCount++;
+	}
+	// Strike Attack / Not Normal Attack, Not Gimic
+	else if (cur_StrikeCount >= max_StrikeCount && cur_SkillCount < max_SkillCount)
+	{
+		bIsAttack = true;
+
+		AttackFunc(3);
+	 	cur_StrikeCount = 0;
+	}
+	// Gimic Attack / Not Normal Attack, Not Strike Attack 
+	else if (cur_StrikeCount < max_StrikeCount && cur_SkillCount >= max_SkillCount)
+	{
+		bIsGimic = true;
+
+		GimicFunc(GetRandomAttackNum(GimicTotalCount));
+		cur_SkillCount = 0;
+	}
+	// Both Gimic Attack and Strike Attack / Not Normal Attack
+	else if (cur_StrikeCount >= max_SkillCount && cur_SkillCount >= max_SkillCount)
+	{
+		bIsAttack = true;
+		bIsGimic = true;
+
+		StrikeGimic(GetRandomAttackNum(GimicTotalCount));
+		cur_StrikeCount = 0;
+		cur_SkillCount = 0;
+	}
+
+	return 0;
+}
+
+void ABoss_TimeMaster::Boss_Death_Implementation()
+{
+	Super::Boss_Death_Implementation();
+}
+
+void ABoss_TimeMaster::AttackFunc_Implementation(int caseNum)
+{
+}
+
+float ABoss_TimeMaster::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+{
+	if (!GetInvincible())
+	{
+		Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+		DamageFlash();
+	}
+
+	CheckCurrentPase();
+
+	return 0.0f;
+}
+
+void ABoss_TimeMaster::DamageFlash_Implementation()
+{
+}
+
+void ABoss_TimeMaster::OnRangeOverlapBegin(UPrimitiveComponent* const OverlappedComponent,
+	AActor* const otherActor, UPrimitiveComponent* const OtherComponent, 
+	int const OtherBodyIndex, bool const FromSweep, FHitResult const& SweepResult)
+{
+}
+
+void ABoss_TimeMaster::OnRangeOverlapEnd(UPrimitiveComponent* const OverlappedComponent,
+	AActor* const otherActor, UPrimitiveComponent* const OtherComponent, int const OtherBodyIndex)
+{
 }
--- Source/TeamChrono/Public/Boss_TimeMaster.h
+++ Source/TeamChrono/Public/Boss_TimeMaster.h
@@ -25,14 +25,121 @@ protected:
 	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 
 public:
+/// Chrono Local Func
+	// Function - Attach Chrono to Cube for AirMoving + Animation Work
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	void SetAttachToPlate();
 
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	void SetFarfromPlayer(float distance);
+
+	UFUNCTION(BlueprintCallable)
+	int GetRandomAttackNum(int num);
+
+	UFUNCTION(BlueprintCallable)
+	void SetFlashMT(class USkeletalMeshComponent* skeleton, int index);
+
+	void CheckCurrentPase();
+
+/// Override Funcs
+	int MeleeAttack_Implementation() override;
+
+	void Boss_Death_Implementation() override;
+
+	/// <summary>
+	/// Chrono Attack Func
+	/// 0 ~ 2 : Normal Attack
+	/// 3 : Strike Attack
+	/// </summary>
+	void AttackFunc_Implementation(int caseNum) override;
+
+	float TakeDamage(float DamageAmount,
+		struct FDamageEvent const& DamageEvent,
+		AController* EventInstigator,
+		AActor* DamageCauser) override;
+
+	void DamageFlash_Implementation() override;
+
+/// Overlap Funcs
+	void OnRangeOverlapBegin(UPrimitiveComponent* const OverlappedComponent,
+		AActor* const otherActor,
+		UPrimitiveComponent* const OtherComponent,
+		int const OtherBodyIndex,
+		bool const FromSweep,
+		FHitResult const& SweepResult) override;
+
+	void OnRangeOverlapEnd(UPrimitiveComponent* const OverlappedComponent,
+		AActor* const otherActor,
+		UPrimitiveComponent* const OtherComponent,
+		int const OtherBodyIndex) override;
+
+/// Calculate Funs
+
+
+
+/// Attack Funcs
+	// Normal Attack - Strike
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	void StrikeAttack();
+
+	// Gimic Attack Funcs
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	void GimicFunc(int num);
+
+	// Gimic Attack After Strike
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	void StrikeGimic(int GimicNum);
 
 private:
+/// SKM
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "SKM", meta = (AllowPrivateAccess = "true"))
+	class USkeletalMeshComponent* sk_Halo;
 
+/// For Attack Properties
+	// Flash MTI
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "DAMAGE FLASH", meta = (AllowPrivateAccess = "true"))
+	class UMaterialInstanceDynamic* FlashMT;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "DAMAGE FLASH", meta = (AllowPrivateAccess = "true"))
+	FLinearColor InitColor;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "DAMAGE FLASH", meta = (AllowPrivateAccess = "true"))
+	float InitMultiplier;
+
+	// Default Properties
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "DEFAULT", meta = (AllowPrivateAccess = "true"))
+	int CurrentPase;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "DEFAULT", meta = (AllowPrivateAccess = "true"))
+	float DistanceToPlayer;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "DEFAULT", meta = (AllowPrivateAccess = "true"))
+	bool bIsAttack;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "DEFAULT", meta = (AllowPrivateAccess = "true"))
+	bool bIsGimic;
+
+	// Normal Attack Properties
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "NORMAL ATTACK", meta = (AllowPrivateAccess = "true"))
+	int NormalAttackTotalCount;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "NORMAL ATTACK", meta = (AllowPrivateAccess = "true"))
+	int cur_StrikeCount;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "NORMAL ATTACK", meta = (AllowPrivateAccess = "true"))
+	int max_StrikeCount;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "NORMAL ATTACK", meta = (AllowPrivateAccess = "true"))
+	int cur_SkillCount;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "NORMAL ATTACK", meta = (AllowPrivateAccess = "true"))
+	int max_SkillCount;
+
+	// Gimic Properties
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "GIMIC", meta = (AllowPrivateAccess = "true"))
+	int GimicTotalCount;
 
 public:
 ///Getter
+	// Default 
+	int GetBossPase() const { return CurrentPase; }
 
+	// Normal Attack
+	int GetNormalAtkType() const { return NormalAttackTotalCount; }
+
+	// Gimic
+	int GetGimicType() const { return GimicTotalCount; }
 
 ///Setter
 
