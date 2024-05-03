24bd314f9734250f36af58ee50f9fa062d589869
20191143namminwoo <mvpalsdn771@gmail.com>
2024-05-03 23:45:25 +0900

    Chrono 작업중 (8)
    1. Chrono_Weapon_ClockPin 생성
     : 각종 함수들 수정 필요

---
+++ Source/TeamChrono/Private/Chrono_Weapon_ClockPin.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Chrono_Weapon_ClockPin.h"
+#include "Components/BoxComponent.h"
+
+// Sets default values
+AChrono_Weapon_ClockPin::AChrono_Weapon_ClockPin()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	BoxColl = CreateDefaultSubobject<UBoxComponent>(TEXT("Box Coll"));
+	RootComponent = BoxColl;
+
+	PinMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Pin Mesh"));
+	PinMesh->SetupAttachment(BoxColl);
+
+	ImplusePoint = CreateDefaultSubobject<USceneComponent>(TEXT("Impulse Point"));
+	ImplusePoint->SetupAttachment(BoxColl);
+
+	ProjectileComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Projectile Component"));
+}
+
+// Called when the game starts or when spawned
+void AChrono_Weapon_ClockPin::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	// For Not Launch Begin
+	ProjectileComponent->InitialSpeed = 0.f;
+	ProjectileComponent->MaxSpeed = 0.f;
+}
+
+// Called every frame
+void AChrono_Weapon_ClockPin::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
