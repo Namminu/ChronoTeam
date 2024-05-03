f2a55ff3a50f090241c14edba9422d6f19596329
20191143namminwoo <mvpalsdn771@gmail.com>
2024-05-03 17:17:42 +0900

    Chrono 작업중 (7)
    Flying 수정중

--- Source/TeamChrono/Private/BTTask_StayFromPlayer.cpp
+++ Source/TeamChrono/Private/BTTask_StayFromPlayer.cpp
@@ -5,6 +5,7 @@
 #include "Boss_TimeMaster.h"
 #include "BossAIController.h"
 #include "BehaviorTree/BlackboardComponent.h"
+#include "Blueprint/AIBlueprintHelperLibrary.h"
 
 UBTTask_StayFromPlayer::UBTTask_StayFromPlayer()
 {
@@ -13,14 +14,23 @@ UBTTask_StayFromPlayer::UBTTask_StayFromPlayer()
 
 EBTNodeResult::Type UBTTask_StayFromPlayer::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	if (auto const* const cont = OwnerComp.GetAIOwner())
+	if (auto* const cont = Cast<ABossAIController>(OwnerComp.GetAIOwner()))
 	{
 		if (auto* const Chrono = Cast<ABoss_TimeMaster>(cont->GetPawn()))
 		{
-			Chrono->SetFarfromPlayer(Chrono->GetPlayerByDistance(), GetRandomFloat(minTime, maxTime));
+			//Chrono->SetFarfromPlayer(Chrono->GetPlayerByDistance(), GetRandomFloat(minTime, maxTime));
+
+
+
+
+			FVector newLocation = FVector(0.f, 0.f, 0.f);
+
+			UAIBlueprintHelperLibrary::SimpleMoveToLocation(cont, newLocation);
+			return EBTNodeResult::Succeeded;
 		}
 	}
-	return EBTNodeResult::Succeeded;
+
+	return EBTNodeResult::Failed;
 }
 
 float UBTTask_StayFromPlayer::GetRandomFloat(float min, float max)
